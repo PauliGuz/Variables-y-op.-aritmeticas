5bd7922491ffca0794da13830c93631249fd7014
PauliGuz <159496881+PauliGuz@users.noreply.github.com>
2024-03-16 13:25:42 -0300

    fix comentario

--- assets/js/temperatura.js
+++ assets/js/temperatura.js
@@ -4,5 +4,5 @@ let celsius = parseInt(prompt("ingrese Grados Celsius"));
 let kelvin = celsius + 273.15;
 //se crea equivalencia fahrenheit celsius
 let fahrenheit = (celsius * 9) / 5 + 32;
-se levanta alerta para que muetre la equivalencia
+//se levanta alerta para que muestre la equivalencia
 alert("Fahrenheit: " + fahrenheit + "\n Kelvin: " + kelvin);
